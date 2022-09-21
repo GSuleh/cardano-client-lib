ade867a9d85dd876824d59e16f40d627e40f5dff
Gerald  Suleh <31475817+GSuleh@users.noreply.github.com>
2022-09-21 10:01:46 +0300

    Update Constants.java
    A proposition to add a prepod URL constant for the blockfrost API.

--- backend-modules/blockfrost/src/main/java/com/bloxbean/cardano/client/backend/blockfrost/common/Constants.java
+++ backend-modules/blockfrost/src/main/java/com/bloxbean/cardano/client/backend/blockfrost/common/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 
     public final static String BLOCKFROST_MAINNET_URL = "https://cardano-mainnet.blockfrost.io/api/v0/";
     public final static String BLOCKFROST_TESTNET_URL = "https://cardano-testnet.blockfrost.io/api/v0/";
+    public final static String BLOCKFROST_PREPOD_URL = "https://cardano-prepod.blockfrost.io/api/v0/";
 }
